2706132ef443a08b6cff40254fbba15648778f19
csm <csm009177@gmail.com>
2025-04-10 23:01:01 +0900

    make base router

--- core/App.php
+++ core/App.php
@@ -4,9 +4,31 @@
 적절한 컨트롤러와 액션으로 라우팅합니다.
 */
 
-class App {
-  public function __construct() {
+class App
+{
+
+  protected $controller = 'homecontroller';
+  protected $method = 'index';
+  protected $params = 'index';
+
+  public function __construct()
+  {
+    $url = $this->parseUrl();
+    if (file_exists('../app/controller/' . $url[0] . '.php')) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+    require_once '../app/controllers' . $this->controller . 'php';
+    $this->controller = new $this->controller;
+
     
   }
 
+  public function parseUrl()
+  {
+
+    if (isset($_GET['url'])) {
+      return explode('/', $_GET['url']);
+    }
+  }
 }
